d80f79e199e7e573704c1dd519fba16fc46377c0
svetlana_korneeva <svet-ko@list.ru>
2022-02-22 17:24:17 +0300

    fix: исправлены ошибки в именовании и структуре

--- scripts/index.js
+++ scripts/index.js
@@ -31,8 +31,10 @@ document.addEventListener('DOMContentLoaded', function() {
   const popupAddPlace = document.querySelector('.popup_type_add-place');
   const popupImageContainer = document.querySelector('.popup_type_photo');
   const popupEdit = document.querySelector('.popup_type_edit');
+  const popupImage = popupImageContainer.querySelector('.popup__image');
+  const popupCaption = popupImageContainer.querySelector('.popup__caption');
 
-  function addElementToElementsList(element) {
+  function createCard(element) {
     const cardTemplate = document.querySelector('#element-template').content;
     const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
     const cardElementName = cardElement.querySelector('.element__name');
@@ -49,28 +51,23 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function removeButtonHandler(evt) {
-      evt.target.parentElement.remove();
-    }
-
-    const popupImage = popupImageContainer.querySelector('.popup__image');
-    const popupCaption = popupImageContainer.querySelector('.popup__caption');
-
-    function copyPhotoToPopup(photo) {
-      popupImage.src = photo.src;
-    }
-
-    function copyCaptionToPopup(name) {
-      popupCaption.textContent = name.textContent;
+      cardElement.remove();
     }
 
     cardElementImage.addEventListener('click', () => {
-        copyPhotoToPopup(cardElementImage);
-        copyCaptionToPopup(cardElementName);
+        popupImage.src = element.link;
+        popupImage.alt = `Картинка "${element.name}"`;
+        popupCaption.textContent = element.name;
         openPopup(popupImageContainer);
     });
 
     likeButton.addEventListener('click', toggleLike);
     removeButton.addEventListener('click', (evt) => removeButtonHandler(evt));
+    return cardElement;
+  }
+
+  function addElementToElementsList(element) {
+    const cardElement = createCard(element);
     elementsList.prepend(cardElement);
   }
 
@@ -102,10 +99,10 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   function addEditPopupEventsListneners(){
-    const closeButton = popupEdit.querySelector('.popup__close-button');
-    const popupForm = popupEdit.querySelector('.popup__form');
-    const nameInput = popupForm.querySelector('.popup__form-item_type_name');
-    const aboutInput = popupForm.querySelector('.popup__form-item_type_about');
+    const profileCloseButton = popupEdit.querySelector('.popup__close-button');
+    const profileForm = popupEdit.querySelector('.popup__form');
+    const nameInput = profileForm.querySelector('.popup__form-item_type_name');
+    const aboutInput = profileForm.querySelector('.popup__form-item_type_about');
 
     const profileName = document.querySelector('.profile__user-name');
     const profileAbout = document.querySelector('.profile__user-about');
@@ -116,23 +113,23 @@ document.addEventListener('DOMContentLoaded', function() {
       aboutInput.value = profileAbout.textContent;
     };
 
-    function editFormSubmitHandler(evt) {
+    function handleEditFormSubmit(evt) {
       evt.preventDefault();
       profileName.textContent = nameInput.value;
       profileAbout.textContent = aboutInput.value;
     }
 
     editButton.addEventListener('click', () => {openPopup(popupEdit); putValue()});
-    closeButton.addEventListener('click', () => closePopup(popupEdit));
-    popupEdit.addEventListener('submit', (evt) => {editFormSubmitHandler(evt); closePopup(popupEdit)});
+    profileCloseButton.addEventListener('click', () => closePopup(popupEdit));
+    popupEdit.addEventListener('submit', (evt) => {handleEditFormSubmit(evt); closePopup(popupEdit)});
     popupEdit.addEventListener('click', (evt) => closePopupByClickOnOverlay(evt, popupEdit));
   }
 
-  function addAddPlacePopupEventsListneners() {
-    const closeButton = popupAddPlace.querySelector('.popup__close-button');
-    const popupForm = popupAddPlace.querySelector('.popup__form');
-    const placeInput = popupForm.querySelector('.popup__form-item_type_place');
-    const urlInput = popupForm.querySelector('.popup__form-item_type_url');
+  function addPlacePopupEventsListneners() {
+    const placePopupCloseButton = popupAddPlace.querySelector('.popup__close-button');
+    const cardForm = popupAddPlace.querySelector('.popup__form');
+    const placeInput = cardForm.querySelector('.popup__form-item_type_place');
+    const urlInput = cardForm.querySelector('.popup__form-item_type_url');
 
     const addButton = document.querySelector('.profile__add-button');
 
@@ -143,27 +140,26 @@ document.addEventListener('DOMContentLoaded', function() {
       return element;
     }
 
-    function addFormSubmitHandler(evt) {
+    function handleCardFormSubmit(evt) {
       evt.preventDefault();
       const newElement = createElement();
       addElementToElementsList(newElement);
-      popupForm.reset();
+      cardForm.reset();
     }
 
     addButton.addEventListener('click', () => openPopup(popupAddPlace));
-    closeButton.addEventListener('click', () => closePopup(popupAddPlace));
-    popupAddPlace.addEventListener('submit', (evt) => {addFormSubmitHandler(evt); closePopup(popupAddPlace)});
+    placePopupCloseButton.addEventListener('click', () => closePopup(popupAddPlace));
+    popupAddPlace.addEventListener('submit', (evt) => {handleCardFormSubmit(evt); closePopup(popupAddPlace)});
     popupAddPlace.addEventListener('click', (evt) => closePopupByClickOnOverlay(evt, popupAddPlace));
   }
 
   function addImagePopupEventsListeners() {
-    const closeButton = popupImageContainer.querySelector('.popup__close-button');
-
-    closeButton.addEventListener('click', () => closePopup(popupImageContainer));
+    const imageCloseButton = popupImageContainer.querySelector('.popup__close-button');
+    imageCloseButton.addEventListener('click', () => closePopup(popupImageContainer));
   }
 
   addElementsFromInitialArray();
   addEditPopupEventsListneners();
-  addAddPlacePopupEventsListneners();
+  addPlacePopupEventsListneners();
   addImagePopupEventsListeners();
 })
